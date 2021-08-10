d92c01c62827bb3de322161dc3929e2ab879a500
Blaise Gerstung <info@clearfeather.com>
2021-08-10 14:23:55 -0500

    update mobile background & links

--- js/mobileMenu.js
+++ js/mobileMenu.js
@@ -1,27 +1,26 @@
 function mobileMenu() {
 
-    //Set Variables for each class style
-
-    //Navigation Menu
+    // Navigation Menu
     const navLinks = document.querySelector("#navLinks");
 
-    //Middle sections
-    //var middleMain = document.getElementsByClassName("middle")[0];
-    //middleMain.classList.toggle("menuBackground")[0];
+    // Middle section - Toggle dark background
+    const middleMain = document.querySelector(".middle");
+    middleMain.classList.toggle('menuBackground');
     
+    // Elements within Middle section (title, buttons, social media)
     const middleTitle = document.querySelector(".title");
     const middleButton = document.querySelector(".button");
     const middleSocial = document.querySelector(".social-buttons");
     
     
-    //Hiding the menu, display middle header
+    // Hiding the menu, display middle header
     if (navLinks.style.display === "flex") {
         navLinks.style.display = "none";
         middleTitle.style.display = "block";
         middleButton.style.display = "block";
         middleSocial.style.display = "flex";
 
-    //Showing the menu, hiding middle header
+    // Showing the menu, hiding middle header
     } else {
         navLinks.style.display = "flex";
         middleTitle.style.display = "none";
